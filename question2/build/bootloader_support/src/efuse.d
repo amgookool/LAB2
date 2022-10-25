390b02f082fbc412b657d48259d0fbd6038bbb4f
Adrian Gookool <amgookool@hotmail.com>
2022-10-24 22:58:56 -0400

    Updated question2 branch with code and output file

--- question2/build/bootloader_support/src/efuse.d
+++
@@ -1,6 +0,0 @@
-src/efuse.o: \
- /home/adrian/esp/ESP8266_RTOS_SDK/components/bootloader_support/src/efuse.c \
- /home/adrian/esp/3006-workspace/Projects/LAB2/question2/build/include/sdkconfig.h
-
-/home/adrian/esp/3006-workspace/Projects/LAB2/question2/build/include/sdkconfig.h:
-/home/adrian/esp/ESP8266_RTOS_SDK/components/bootloader_support/src/efuse.c:
