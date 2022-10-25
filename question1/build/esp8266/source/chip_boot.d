f210fcb5b25da6420931a3d6731f182ad2a831a5
Adrian Gookool <amgookool@hotmail.com>
2022-10-24 21:26:40 -0400

    Cleaned up repository for Question3 branch

--- question1/build/esp8266/source/chip_boot.d
+++
@@ -1,6 +0,0 @@
-source/chip_boot.o: \
- /home/adrian/esp/ESP8266_RTOS_SDK/components/esp8266/source/chip_boot.c \
- /home/adrian/esp/3006-workspace/Projects/LAB2/question1/build/include/sdkconfig.h
-
-/home/adrian/esp/3006-workspace/Projects/LAB2/question1/build/include/sdkconfig.h:
-/home/adrian/esp/ESP8266_RTOS_SDK/components/esp8266/source/chip_boot.c:
--- question2/build/json/cJSON/cJSON.d
+++
@@ -1,6 +0,0 @@
-cJSON/cJSON.o: \
- /home/adrian/esp/ESP8266_RTOS_SDK/components/json/cJSON/cJSON.c \
- /home/adrian/esp/ESP8266_RTOS_SDK/components/json/cJSON/cJSON.h
-
-/home/adrian/esp/ESP8266_RTOS_SDK/components/json/cJSON/cJSON.h:
-/home/adrian/esp/ESP8266_RTOS_SDK/components/json/cJSON/cJSON.c:
